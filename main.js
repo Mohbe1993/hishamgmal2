0de214808b920b61161245bc1bafd2eb712f83cb
Mohbe1993 <mohamedbehairy1993@yahoo.com>
2025-05-06 02:48:18 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector(".img0");
+let cont = document.querySelector(".cont");
+
+function phones(phone) {
+  img.src = phone;
+}
+
+function colors(color) {
+  cont.style.background = color;
+}
